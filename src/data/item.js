0679c8f8d30bc1f9c84c5495f294f8dba85de0c3
dadyday <github.com@holgerheyne.de>
2021-07-05 07:04:12 +0200

    extend database

--- src/data/item.js
+++ src/data/item.js
@@ -1,5 +1,6 @@
 
 export default {
+// iron tier 0
 	ironOre:     ['Eisenerz', 'belt', 100],
 	copperOre:   ['Kupfererz', 'belt', 100],
 	ironIngot:   ['Eisenbarren', 'belt', 100],
@@ -7,7 +8,17 @@ export default {
 	ironPlate:   ['Eisenplatte', 'belt', 100],
 	ironRod:     ['Eisenstange', 'belt', 100],
 	screw:       ['Schrauben', 'belt', 100],
+
+// copper tier 0
 	wire:        ['Draht', 'belt', 100],
 	cable:       ['Kabel', 'belt', 100],
 	reinforced:  ['Verstärkte Eisenplatte', 'belt', 100, 'reinforcedIronPlate'],
+
+
+	crystalOscillator: ['Crystal Oszillator', 'belt', 100],
+	circuitBoard:      ['Schaltkreis', 'belt', 100],
+	aluminumCasing:    ['Aluminium Gehäuse', 'belt', 100],
+	rubber:            ['Gummi', 'belt', 100],
+
+	radioControlUnit:  ['Radioeinheit', 'belt', 100],
 }
